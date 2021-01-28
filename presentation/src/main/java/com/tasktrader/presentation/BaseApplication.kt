cef4a6c4b45354e4480c8a5b311d5676c56e4a19
Kirsten <gk.niebuhr@gmail.com>
2021-01-28 18:50:42 -0300

    Added firebase and updated gradle

--- presentation/src/main/java/com/tasktrader/presentation/BaseApplication.kt
+++ presentation/src/main/java/com/tasktrader/presentation/BaseApplication.kt
@@ -1,6 +1,7 @@
 package com.tasktrader.presentation
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import com.tasktrader.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -10,6 +11,8 @@ class BaseApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        FirebaseApp.initializeApp(this)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
